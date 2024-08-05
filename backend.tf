625adf2cdab1eec4040092b03111921d5f446d44
ZARA REZAIE <thoughtworks.rezaie@extaccount.com>
2024-08-05 17:27:59 +0200

    fix s3 bucket name error

---
+++ backend.tf
@@ -0,0 +1,34 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "aws-nlp-pipeline-state-bucket"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    Name = "AWS NLP Pipeline Terraform State Bucket"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "aws-nlp-pipeline-terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "AWS NLP Pipeline Terraform Lock Table"
+  }
+}
