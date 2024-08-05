e03a823e1f69cb738e823b303c2ee012d1dd41a0
ZARA REZAIE <thoughtworks.rezaie@extaccount.com>
2024-08-05 16:19:23 +0200

    add region and account id variables

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "aws_account_id" {
+  description = "Your AWS Account ID"
+  type        = string
+  default     = "970837487748"
+}
